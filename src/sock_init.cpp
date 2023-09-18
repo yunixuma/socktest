91f539c3d9544b038af6f23016942e4ec3f2c921
ykosaka <ykosaka@student.42tokyo.jp>
2023-09-18 18:02:22 +0900

    Modify server (divide funcs)

--- src/sock_init.cpp
+++ src/sock_init.cpp
@@ -2,9 +2,7 @@
 
 #include <sock_common.h>
 
-int	sock_init(t_addrin *addr, int port_num) {
-	Debug::print("si0	addr", addr);
-
+static int	sock_init_fd(void) {
 	//ソケットの生成
 	int fd_sock = socket(AF_INET, SOCK_STREAM, 0); //アドレスドメイン, ソケットタイプ, プロトコル
 	if (fd_sock < 0) { //エラー処理
@@ -14,12 +12,26 @@ int	sock_init(t_addrin *addr, int port_num) {
 	}
 	Debug::print("si2\tfd_sock", fd_sock);
 
+	return (fd_sock);
+}
+
+static void	sock_init_addr(t_addrin *addr, int port_num) {
 	//アドレスの生成
 	memset(addr, 0, sizeof(t_addrin)); //memsetで初期化
 	addr->sin_family = AF_INET; //アドレスファミリ(ipv4)
 	addr->sin_addr.s_addr = inet_addr(IP_LOCAL); //IPアドレス,inet_addr()関数はアドレスの翻訳
 	addr->sin_port = htons(port_num); //ポート番号,htons()関数は16bitホストバイトオーダーをネットワークバイトオーダーに変換
 	Debug::print("si4\taddr", addr);
+}
+
+int	sock_init(t_addrin *addr, int port_num) {
+	Debug::print("si0	addr", addr);
+
+	//ソケットの生成
+	int fd_sock = sock_init_fd();
+
+	//アドレスの生成
+	sock_init_addr(addr, port_num);
 
 	return (fd_sock);
 }
--- src/sock_server.cpp
+++ src/sock_server.cpp
@@ -2,10 +2,55 @@
 
 #include <sock_server.h>
 
-int	main(int argc, char *argv[]) {
+static int	sock_setport(int argc, char *argv[]) {
 	int	port_num = NUM_INVALID;
 	if (argc > 1)
 		port_num = ft_stoi(argv[1]);
+	if (port_num == NUM_INVALID)
+		port_num = PORT_NUM;
+	return (port_num);
+}
+
+static void	sock_listen(int fd_sock) {
+	//受信待ち
+	int	status_listen = listen(fd_sock, SOMAXCONN);
+	if (status_listen < 0) { //ソケット, キューの最大長 //エラー処理
+		std::cerr << COLOR_RED << "Error listen: " \
+			<< std::strerror(errno) << std::endl; //標準エラー出力
+		close(fd_sock); //ソケットクローズ
+		exit(1); //異常終了
+	}
+}
+
+static int	sock_accept(int fd_sock) {
+	t_addrin addr_to; //接続相手のソケットアドレス
+	socklen_t len_addr = sizeof(t_addrin); //接続相手のアドレスサイズ
+	Debug::print("sm41\taddr_to", &addr_to);
+	Debug::print("sm41\tlen_addr", len_addr);
+	//接続待ちソケット, 接続相手のソケットアドレスポインタ, 接続相手のアドレスサイズ
+	int	fd_conn = accept(fd_sock, (sockaddr *)&addr_to, &len_addr);
+	Debug::print("sm41\tfd_conn", fd_conn);
+	if (fd_conn < 0) { //エラー処理
+		std::cerr << COLOR_RED << "Error accept: " << std::strerror(errno) << std::endl; //標準エラー出力
+		exit(1); //異常終了
+	}
+	std::clog << COLOR_GREEN << "connected" << std::endl; //標準エラー出力
+	return (fd_conn);
+}
+
+static void	sock_recv(int fd_conn) {
+	//受信
+	size_t len_msg = LENGTH_MSG;
+	char msg[len_msg]; //受信用データ格納用
+	int	i = 0;
+	while (i++ < NUM_CYCLE) {
+		recv(fd_conn, msg, len_msg, 0); //受信
+		std::cout << msg << std::endl; //標準出力
+	}
+}
+
+int	main(int argc, char *argv[]) {
+	int	port_num = sock_setport(argc, argv);
 	if (port_num == NUM_INVALID)
 		port_num = PORT_NUM;
 
@@ -16,38 +61,17 @@ int	main(int argc, char *argv[]) {
 	fd_sock = sock_init(&addr_from, port_num);
 
 	//受信待ち
-	int	status_listen = listen(fd_sock, SOMAXCONN);
-	if (status_listen < 0) { //ソケット, キューの最大長 //エラー処理
-		std::cerr << COLOR_RED << "Error listen: " << std::strerror(errno) << std::endl; //標準エラー出力
-		close(fd_sock); //ソケットクローズ
-		exit(1); //異常終了
-	}
+	sock_listen(fd_sock);
 
 	//接続待ち
-	struct sockaddr_in addr_to; //接続相手のソケットアドレス
-	socklen_t len_addr = sizeof(t_addrin); //接続相手のアドレスサイズ
-	Debug::print("sm41\taddr_to", &addr_to);
-	Debug::print("sm41\tlen_addr", len_addr);
-	int	fd_conn = accept(fd_sock, (t_addr *)&addr_to, &len_addr); //接続待ちソケット, 接続相手のソケットアドレスポインタ, 接続相手のアドレスサイズ
-	Debug::print("sm41\tfd_conn", fd_conn);
-	if (fd_conn < 0) { //エラー処理
-		std::cerr << COLOR_RED << "Error accept: " << std::strerror(errno) << std::endl; //標準エラー出力
-		exit(1); //異常終了
-	}
-	std::clog << COLOR_GREEN << "connected" << std::endl; //標準エラー出力
+	int	fd_conn = sock_accept(fd_sock);
 
 	//受信
-	size_t len_msg = LENGTH_MSG;
-	char msg[len_msg]; //受信用データ格納用
-	int	i = 0;
-	while (i++ < NUM_CYCLE) {
-		recv(fd_conn, msg, len_msg, 0); //受信
-		std::cout << msg << std::endl; //標準出力
-	}
+	sock_recv(fd_conn);
 
 	//送信
-	send(fd_conn, msg, len_msg, 0); //送信
-	std::cout << msg << std::endl; //標準出力
+	// send(fd_conn, msg, len_msg, 0); //送信
+	// std::cout << msg << std::endl; //標準出力
 
 	//ソケットクローズ
 	close(fd_conn);
