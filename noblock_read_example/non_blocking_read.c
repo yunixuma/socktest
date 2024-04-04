1fdaa6659d75d89c401c782d9937876580d213f2
Yoshihiro Kosaka <ykosaka@student.42tokyo.jp>
2024-04-05 04:10:16 +0900

    Add non-blocking-read

---
+++ noblock_read_example/non_blocking_read.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+int	main(void) {
+	// ファイルディスクリプタをノンブロッキングに設定
+	int	flags = 0;
+
+	if ((flags = fcntl(STDIN_FILENO, F_GETFL)) == -1) {
+		perror("fcntl");
+		exit(EXIT_FAILURE);
+	}
+	flags |= O_NONBLOCK;
+	if (fcntl(STDIN_FILENO, F_SETFL, flags) == -1) {
+		perror("fcntl");
+		exit(EXIT_FAILURE);
+	}
+	while (1) {
+		sleep(3);
+
+		// char	buffer[1024] = {0};
+		char	buffer[4] = {0};
+		ssize_t	bytes_read = 0;
+
+		errno = 0;
+
+		// ノンブロッキングなファイルディスクリプタからデータを読み取る
+		bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer));
+
+		if (bytes_read == -1) {
+			perror("read");
+			if (errno == EAGAIN || errno == EWOULDBLOCK) {
+				continue ;
+			} else {
+				exit(EXIT_FAILURE);
+			}
+		} else if (bytes_read > 0) {
+			// データが読み取られた場合
+			printf("Read %zd bytes: %s", bytes_read, buffer);
+		} else {
+			// データがない場合
+			printf("No data available.\n");
+			break ;
+		}
+	}
+	return (0);
+}
